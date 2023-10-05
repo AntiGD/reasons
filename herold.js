012167572a855413a1dafd4e9a58c41c7146217d
AntiGD <141681425+AntiGD@users.noreply.github.com>
2023-10-05 15:14:12 -0500

    Update herold.js

--- herold.js
+++ herold.js
@@ -1,5 +1,14 @@
-var ad = document.getElementById("ad");
-var ads = 40;
-var randomthing = Math.floor(Math.random() * ads) + 1;
-var chosenad = "ad" + randomthing + ".gif";
-ad.style.backgroundImage = "url('" + chosenad + "')";
+function getRandomAdImage() {
+            const numberOfAds = 40;
+            const randomIndex = Math.floor(Math.random() * numberOfAds) + 1;
+            return `ads/ad${randomIndex}.gif`;
+}
+
+window.addEventListener('load', function () {
+            const ad = document.getElementById('ad');
+            const ad2 = document.getElementById('ad2');
+            const topad = document.getElementById('topad');
+            ad.src = getRandomAdImage();
+            ad2.src = getRandomAdImage();
+            topad.src = getRandomAdImage();
+});
